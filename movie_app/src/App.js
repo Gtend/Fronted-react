ceaa68a43a480be7a3e31ec9d124f6ced5aea5cc
Gtend <86248339+Gtend@users.noreply.github.com>
2024-11-16 18:26:42 +0900

    component Life Cycle

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,51 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+ 
+/* componetn life cycle
 
-// 컴포넌트
-// props == props.fav == {fav}
-function Food({name, picture}) {
-  console.log(name);
-  console.log(picture)
-  return <div>
-        <h1>I like {name}</h1>
-        <img src = {picture}/>
+  mounting (생성)
+   - constuctor
+   - render
+   - componentDidMount (컴포넘트 처음 만들어질떄)
+  updating (변경)
+   - componentDidUpdate
+  unmounting (삭제)
+   - componentWillUnmount
+
+*/
+
+// class component
+// dont have return, have render method
+// 자동적으로 클래스 컴포넘트안에 있는 render를 실행해줌
+// 변하는 데이터는 state안에 저장
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log("start")
+  }
+
+  // state object
+  state = {
+    count: 0
+  }
+
+  // add func
+  add = () => {
+    console.log("add");
+    //this.state.count = 1; <-이거 동작 안함
+    // 아래 처럼(setState) 해야 render 다시 호출해서 반영해줌 ㅇㅇ why? virtual DOM 갖고있어서,,
+    // this.setState({ count: 1 })
+    // 이것도 좋은 방법 아님..
+    //this.setState({ count: this.state.count + 1 })
+
+    // best state 이용법
+    this.setState(current => ({count: current.count + 1}));
+  };
+
+  // minus func
+  minus = () => {
+    console.log("minus")
+    this.setState(current => ({count: current.count - 1}));
+  };
+
+  componentDidMount() {
+    console.log("component Mount")
+  }
+
+  componentDidUpdate() {
+    console.log("component update")
+  }
+
+  componentWillUnmount() {
+    console.log("died")
+  }
+
+  // render func
+  render() {
+    console.log("render func")
+    return (
+      <div>
+        <h1>Im a class component, the number is {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
-}
-
-const foodILike = [
-  {
-    name : "kimchi",
-    image : "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004"
-  },
-  {
-    name : "Samgyeopsal",
-    image : "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004"
-  },
-  {
-    name : "Bibimbap",
-    image : "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004"
-  },  
-
-]
-
-function renderFood(dish) {
-  console.log(dish)
-  return <Food name={dish.name} picture={dish.image} />
-}
-function App() {
-  return (
-  <div>
-    <h1>Hello!!</h1>
-    <Food name="kimchi"/>
-    <Food name="ramen"/>
-    <Food name="samgiopsal"/>
-
-    {foodILike.map(dish => (
-      <Food name={dish.name} picture={dish.image}/> 
-      ))}
-    {foodILike.map(renderFood)}
-  </div>
-  );
+    )
+  }
 }
 
 export default App;
