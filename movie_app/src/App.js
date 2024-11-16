1682159fddad8be426fe4db3c669ac4bc134e5d1
Gtend <86248339+Gtend@users.noreply.github.com>
2024-11-16 17:33:50 +0900

    component

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 컴포넌트
+// props == props.fav == {fav}
+function Food({name, picture}) {
+  console.log(name);
+  console.log(picture)
+  return <div>
+        <h1>I like {name}</h1>
+        <img src = {picture}/>
+      </div>
+}
+
+const foodILike = [
+  {
+    name : "kimchi",
+    image : "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004"
+  },
+  {
+    name : "Samgyeopsal",
+    image : "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004"
+  },
+  {
+    name : "Bibimbap",
+    image : "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004"
+  },  
+
+]
+
+function renderFood(dish) {
+  console.log(dish)
+  return <Food name={dish.name} picture={dish.image} />
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Hello!!</h1>
+    <Food name="kimchi"/>
+    <Food name="ramen"/>
+    <Food name="samgiopsal"/>
+
+    {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image}/> 
+      ))}
+    {foodILike.map(renderFood)}
+  </div>
   );
 }
 
